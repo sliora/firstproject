9565bead6ea8953c7b2c44e59efa7d2dce7981a1
김영회 <zerom@jeongjaehuiui-MacBookAir.local>
2022-01-15 10:14:12 +0900

    Add controller

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -13,4 +13,10 @@ public class FirstController {
         return "greetings"; // templates/greetings.mustache -> 브라우저로 전송!
     }
 
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname","홍길동");
+        return "goodbye";
+    }
+
 }
