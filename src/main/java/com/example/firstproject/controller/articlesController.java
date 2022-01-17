ca36539a14d1ec47fe208080e025daa02806ebc0
zerom-H <cupdisin@gmail.com>
2022-01-17 23:53:51 +0900

    Data Table Create with JPA

---
+++ src/main/java/com/example/firstproject/controller/articlesController.java
@@ -0,0 +1,35 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class articlesController {
+
+    @Autowired // 스프링 부트가 미리 생성해놓은 객체를 가져다가 자동으로 연결 = new ArticleRepositoryimpl 을 안써도 됨
+    private ArticleRepository articleRepository;
+
+    @GetMapping("articles/new")
+    public String newArticlesForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form.toString());
+
+        // 1. Dto를 변환 -> Entity
+        Article article = form.toEntitiy();
+        System.out.println(article.toString());
+
+        // 2. Reposotory에게 Entity를 DB안에 저장하게 함
+        Article saved = articleRepository.save(article);
+        System.out.println(saved.toString());
+        return "";
+    }
+}
