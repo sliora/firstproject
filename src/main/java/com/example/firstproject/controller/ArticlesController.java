b68b7656e9bd156b254846f846bbeb6d61a91817
zerom-H <cupdisin@gmail.com>
2022-01-19 01:07:33 +0900

    Template Engine Practice

--- src/main/java/com/example/firstproject/controller/ArticlesController.java
+++ src/main/java/com/example/firstproject/controller/ArticlesController.java
@@ -6,8 +6,10 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j //로깅을 위한 어노테이션
@@ -36,5 +38,12 @@ public class ArticlesController {
         //System.out.println(saved.toString());
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloSpring(@RequestParam("name") String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
