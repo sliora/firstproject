575e39c906e634480a514a2a09998eb7b6572756
zerom-H <cupdisin@gmail.com>
2022-01-29 22:20:49 +0900

    SpringConfig Add

---
+++ src/main/java/com/example/firstproject/service/SpringConfig.java
@@ -0,0 +1,21 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.repository.MemberRepository;
+import com.example.firstproject.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+}
