467fed5d6d41c8782e7762e30fa3d224d7cb7ae7
zerom-H <cupdisin@gmail.com>
2022-02-03 21:47:56 +0900

    JDBC ADD

--- src/main/java/com/example/firstproject/service/SpringConfig.java
+++ src/main/java/com/example/firstproject/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.repository.JdbcMemberRepository;
 import com.example.firstproject.repository.MemberRepository;
 import com.example.firstproject.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,7 +26,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 
 }
